eb9319a292b397f546cc34c0798244ea9ba7b3f1
mohamedfarith <mohamedfarith96@gmail.com>
2018-07-18 10:59:40 +0530

    Revert "Made changes to the Activities as per naming conventions"
    This reverts commit 499c2ce8ac923e31575857a864925993cf7d2405.

--- app/src/main/java/com/example/admin/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/admin/learningandroid/MainActivity.java
@@ -7,24 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonActivity;
-    Button buttonLayout;
+    Button activity_button, layout_button, fragment_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-        buttonActivity = findViewById(R.id.activityButton);
-        buttonLayout = findViewById(R.id.layoutButton);
+        setContentView(R.layout.layout);
+        activity_button = (Button) findViewById(R.id.activity_button);
+        layout_button = (Button) findViewById(R.id.layout_button);
+        fragment_button = (Button) findViewById(R.id.fragment_button);
 
 
-        buttonActivity.setOnClickListener(new View.OnClickListener() {
+        activity_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LearningActivity.class);
+                Intent intent = new Intent(MainActivity.this, Activity_of_Learning_Activity.class);
                 startActivity(intent);
             }
         });
-
+        layout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity_of_Layout_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
