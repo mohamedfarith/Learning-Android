8810f550cb334ee360bfdcf7167e6bcf4d105e73
mohamedfarith <mohamedfarith96@gmail.com>
2018-08-30 09:37:09 +0530

    Different ways of threading in two activity app

--- app/src/main/java/com/example/admin/learningandroid/Constants.java
+++ app/src/main/java/com/example/admin/learningandroid/Constants.java
@@ -3,5 +3,6 @@ package com.example.admin.learningandroid;
 public class Constants {
     public static final String KEY_NAME = "Name";
     public static final String LIST = "Name List";
+    public static final String THREAD = "ThreadActivityExtending";
 
 }
--- app/src/main/java/com/example/admin/learningandroid/layouts/ModelLoginPage.java
+++ app/src/main/java/com/example/admin/learningandroid/layouts/ModelLoginPage.java
@@ -1,5 +1,6 @@
 package com.example.admin.learningandroid.layouts;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.ColorSpace;
@@ -23,6 +24,7 @@ public class ModelLoginPage extends AppCompatActivity {
     String mName;
     String mRegistrationNumber;
     public String name;
+    public static final double LENGTH_S = 0.2;
 
 
     @Override
@@ -33,27 +35,33 @@ public class ModelLoginPage extends AppCompatActivity {
         etRegistrationNumber = findViewById(R.id.et_registration);
         btnSignUp = findViewById(R.id.btn_sign_up);
         btnLogin = findViewById(R.id.btn_login);
-        
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 //To make only one time sign up
                 SharedPreferences firstRetrieve = getSharedPreferences("SAVE_DATA", MODE_PRIVATE);
                 String savedName = firstRetrieve.getString("NAME", "");
                 String savedRegNumber = firstRetrieve.getString("REGISTRATION_NUMBER", "");
 
-                mName = etName.getText().toString();
-                mRegistrationNumber = etRegistrationNumber.getText().toString();
+                mName = etName.getText().toString().trim();
+                mRegistrationNumber = etRegistrationNumber.getText().toString().trim();
 
                 int mCount = mName.length();
                 //condition for various credential check
                 if (mCount <= 3) {
-                    Toast.makeText(ModelLoginPage.this, "Enter Valid Name", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(ModelLoginPage.this, "Enter Valid Name",Toast.LENGTH_SHORT).show();
+                    Context context = getApplicationContext();
+                    CharSequence text = "Enter Valid Name";
+                    int duration = 1000;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 } else {
                     if (mRegistrationNumber.length() < 1) {
                         Toast.makeText(ModelLoginPage.this, "enter valid register number", Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(ModelLoginPage.this, "Successfully saved", Toast.LENGTH_SHORT).show();
+
                         if (savedName.equals("") && savedRegNumber.equals("")) {
 
                             //Loading data in shared preference
@@ -62,11 +70,13 @@ public class ModelLoginPage extends AppCompatActivity {
                             addData.putString("NAME", mName);
                             addData.putString("REGISTRATION_NUMBER", mRegistrationNumber);
                             addData.apply();
+                            Toast.makeText(ModelLoginPage.this, "Successfully saved", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(ModelLoginPage.this, "Already signed up it seems", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
+                Toast.makeText(ModelLoginPage.this, "hai", Toast.LENGTH_SHORT).show();
             }
 
 
@@ -75,14 +85,15 @@ public class ModelLoginPage extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(ModelLoginPage.this, "hai hello", Toast.LENGTH_SHORT).show();
 
                 //Retrieving shared preference data
                 SharedPreferences preference = getSharedPreferences("SAVE_DATA", MODE_PRIVATE);
                 String name = preference.getString("NAME", "");
                 String registrationNumber = preference.getString("REGISTRATION_NUMBER", "");
 
-             //making sure that the user do not logs in with empty data
-                if (name!="" && registrationNumber!="") {
+                //making sure that the user do not logs in with empty data
+                if (name != "" && registrationNumber != "") {
                     if (name.equals(etName.getText().toString())) {
                         if (registrationNumber.equals(etRegistrationNumber.getText().toString())) {
                             Toast.makeText(ModelLoginPage.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
