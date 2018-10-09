4a5c759563e3954b2e5dc81df8bd67de347284ee
mohamedfarith <mohamedfarith96@gmail.com>
2018-10-09 07:18:44 +0530

    -->As all the files under ".idea/" should be under gitignore file,added .idea/caches/build_file_checksums -->Duplicate variables for SharedPreference,name and registration number is removed. -->isEmpty is replaced by TextUtils.isEmpty.

--- app/src/main/java/com/example/admin/learningandroid/layouts/ModelLoginActivity.java
+++ app/src/main/java/com/example/admin/learningandroid/layouts/ModelLoginActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.learningandroid.layouts;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,7 @@ public class ModelLoginActivity extends AppCompatActivity implements View.OnClic
     public Button btnLogin;
     String mName;
     String mRegistrationNumber;
+    SharedPreferences userPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +39,25 @@ public class ModelLoginActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
+        //variables that is used to store the userPreference data
+        String userName;
+        String userRegistrationNumber;
         switch (view.getId()) {
             case R.id.btn_sign_up:
 
-                SharedPreferences userPreference = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
-                String savedName = userPreference.getString(NAME, "");
-                String savedRegNumber = userPreference.getString(REGISTRATION_NUMBER, "");
-                 mName = etName.getText().toString().trim();
+                userPreference = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
+                userName = userPreference.getString(NAME, "");
+                userRegistrationNumber = userPreference.getString(REGISTRATION_NUMBER, "");
+                mName = etName.getText().toString().trim();
                 mRegistrationNumber = etRegistrationNumber.getText().toString();
                 String message = "";
                 if (mName.length() <= 3) {
                     message = "Enter Valid name";
-                } else if (mRegistrationNumber.isEmpty()) {
+                } else if (TextUtils.isEmpty(mRegistrationNumber)) {
                     message = "enter valid register number";
-                } else if(savedName.isEmpty()&&savedRegNumber.isEmpty()){
-                    SharedPreferences saveData = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
-                    SharedPreferences.Editor addData = saveData.edit();
+                } else if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(userRegistrationNumber)) {
+                    userPreference = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
+                    SharedPreferences.Editor addData = userPreference.edit();
                     addData.putString(NAME, mName);
                     addData.putString(REGISTRATION_NUMBER, mRegistrationNumber);
                     addData.apply();
@@ -63,13 +68,13 @@ public class ModelLoginActivity extends AppCompatActivity implements View.OnClic
                 Toast.makeText(ModelLoginActivity.this, message, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_login:
-                SharedPreferences preference = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
-                String name = preference.getString(NAME, "");
-                String registrationNumber = preference.getString(REGISTRATION_NUMBER, "");
+                userPreference = getSharedPreferences(SAVE_DATA, MODE_PRIVATE);
+                userName = userPreference.getString(NAME, "");
+                userRegistrationNumber = userPreference.getString(REGISTRATION_NUMBER, "");
                 //making sure that the user do not logs in with empty data
 
-                if (!name.isEmpty() && !registrationNumber.isEmpty()) {
-                    if (name.equals(etName.getText().toString().trim()) && (registrationNumber.equals(etRegistrationNumber.getText().toString()))) {
+                if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(userRegistrationNumber)) {
+                    if (userName.equals(etName.getText().toString().trim()) && (userRegistrationNumber.equals(etRegistrationNumber.getText().toString()))) {
                         message = "Successfully logged in";
                     } else {
                         message = "invalid login";
